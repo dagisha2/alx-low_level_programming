5e3f161a4af57d375b12d25778cd49bed52516c6
dagisha2 <getishdagisha@gmail.com>
2022-06-30 16:02:47 -0700

    puttt

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,11 @@
 #include <stdio.h>
+
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	char A[100] = "\"Programming is like building a multilingual puzzle";
