f00e014f6c441feecf79c0f392fc9aef87891eee
dagisha2 <getishdagisha@gmail.com>
2022-06-30 16:41:27 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,14 @@
-#
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char S[100] = "with proper grammar, but the outcome is a piece of art,";
+
+	printf("%s", S);
+
+	return (0);
+}
