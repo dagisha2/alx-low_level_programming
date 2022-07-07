c3a1111481d63246a122f424dcdef1832b1830c1
dagisha2 <getishdagisha@gmail.com>
2022-07-06 19:11:29 -0700

    djfbfmvdvsdfgisd

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+* times_table - print 9 times table
+*
+* Return: Nothing
+*/
+
+void times_table(void)
+
+{
+
+	int i, j, n;
+for (i = 0; i <= 9; i++)
+{
+for (j = 0; j <= 9; j++)
+{
+n = i * j;
+if (j != 0)
+{
+_putchar(',');
+_putchar(' ');
+}
+if (n >= 10)
+{
+_putchar((n / 10) + '0');
+_putchar((n % 10) + '0');
+}
+else if (n < 10 && j != 0)
+{
+_putchar(' ');
+_putchar((n % 10) + '0');
+}
+else
+{
+_putchar((n % 10) + '0');
+}
+}
+_putchar('\n');
+}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -12,4 +12,6 @@ int _abs(int);
 int add(int, int);
 int print_last_digit(int);
 void jack_bauer(void);
+void print_to_98(int n);
+void times_table(void);
 #endif
