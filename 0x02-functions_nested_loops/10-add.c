2e1974b531a1125b20ac9e5e90b540287affb74e
dagisha2 <getishdagisha@gmail.com>
2022-07-06 17:33:19 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - adding two numbers
+ * @a: first no
+ * @b: second no
+ *
+ * Return: 0
+*/
+int add(int a, int b)
+{
+	return (a + b);
+}
---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - absolute value of a number
+ * @d: argument
+ *
+ * Return: 0
+ */
+int _abs(int d)
+{
+if (d < 0)
+{
+d = -d;
+}
+return (0);
+}
+
