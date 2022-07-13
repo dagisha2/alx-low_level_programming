6dba3d93372fb5618bf54ad95e527517a215d37e
dagisha2 <getishdagisha@gmail.com>
2022-07-13 11:30:00 -0700

    aas

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* positive_or_negative - print sign
+* @i: argu
+*
+* Return: 0
+*/
+void positive_or_negative(int i)
+{
+if (i > 0)
+{
+printf("%d is positive", i);
+}
+else if (i < 0)
+{
+printf("%d is negative", i);
+}
+else
+{
+printf("%d is zero", i);
+}
+putchar('\n');
+}
