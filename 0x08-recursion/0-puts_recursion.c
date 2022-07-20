a68e27a5bc86c9d4555f75c79e766a14cc70665d
dagisha2 <getishdagisha@gmail.com>
2022-07-20 08:04:05 -0700

    recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - printing recursively
+ * @s: input pointer character
+ *
+ * Return: nothing
+*/
+void _puts_recursion(char *s)
+{
+if (*s != '\0')
+{
+_putchar(*(s + 0));
+_puts_recursion((s + 1));
+}
+else
+{
+_putchar('\n');
+return ;
+}
+}
