1a7d24e6b0759cab55b4da7180a64c32cc85b7bd
dagisha2 <getishdagisha@gmail.com>
2022-07-21 16:18:34 -0700

    pow

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -9,17 +9,17 @@
 */
 int _pow_recursion(int x, int y)
 {
-if (y != 0)
+if (y > 0)
 {
 return (x * _pow_recursion(x, y - 1));
 }
-if ((y = 0))
+if (y < 0)
 {
-return (x);
+return (-1);
 }
 else
 {
-return ;
+return (1);
 }
 return (0);
 }
