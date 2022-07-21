0ff06e3aee765132bc35cbe0a53a4c6b88fe853e
dagisha2 <getishdagisha@gmail.com>
2022-07-21 15:26:51 -0700

    recrevstr

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -8,14 +8,13 @@
 */
 void _print_rev_recursion(char *s)
 {
-if (*s < '\0')
+if (*s)
+{
+_print_rev_recursion((s + 1));
+_putchar(*(s + 0));
+}
+else
 {
 return;
-}
-else 
-{
-_putchar(*s);
-_print_rev_recursion((s-1));
-_print_rev_recursion(s);
 }
 }
