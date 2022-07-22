45204aba6495884c4b35a2f1f91fed8795a28c41
dagisha2 <getishdagisha@gmail.com>
2022-07-22 13:12:37 -0700

    sd

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _islower - checks lowercase alphabet
+ * @c: character to be checked
+ *
+ * Return:1 otherwise 0
+*/
+int _islower(int c)
+{
+return (c >= 'a' && c <= 'z');
+}
+
+
+
---
+++ 0x09-static_libraries/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isalpha - check for letter
+ * @c: character to be checked
+ *
+ * Return:1 otherwise 0
+*/
+int _isalpha(int c)
+{
+	return (c >= 65 && c <= 122);
+}
+
+
