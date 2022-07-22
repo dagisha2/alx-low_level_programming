c74102d80daae6b4feca63b8186f9ebc1ac963bd
dagisha2 <getishdagisha@gmail.com>
2022-07-22 13:18:05 -0700

    sda

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _isupper - check for upper case
+ * @c: argu
+ *
+ * Return: 1 for upper case else 0
+*/
+int _isupper(int c)
+{
+return (c >= 65 && c <= 90);
+}
+
---
+++ 0x09-static_libraries/1-isdigit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _isdigit - check for digit
+ * @c: agru
+ *
+ * Return: 1 else 0
+*/
+int _isdigit(int c)
+{
+	return (c >= 48 && c <= 57);
+}
+
