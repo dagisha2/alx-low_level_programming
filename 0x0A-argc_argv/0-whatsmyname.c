ec09fb990287b7820832c10a5f7141fd0b609fde
dagisha2 <getishdagisha@gmail.com>
2022-07-27 11:55:25 -0700

    gchf

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * main - print the name of program
+ * @argc: number of command line arguments
+ * @argv: input array
+ *
+ * Return: 0
+*/
+int main(int argc, char* argv[])
+{
+argv[0];
+(void)argc;
+return (0);
+}
