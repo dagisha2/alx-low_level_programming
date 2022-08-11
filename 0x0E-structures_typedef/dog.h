c12d439b74e8e08768e7500c1aa43068ad2a3608
dagisha2 <getishdagisha@gmail.com>
2022-08-11 01:46:32 -0700

    betty

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,9 +1,18 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+ * struct dog - user defined variable
+ * @name: attribute1
+ * @owner: attribute2
+ * @age: attribute3
+ */
 struct dog
-{ char* name;
-  char* owner;
-  float age;
+{
+char *name;
+
+char *owner;
+
+float age;
 };
 
 #endif
